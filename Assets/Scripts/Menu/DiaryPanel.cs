3b4916f39ad3c63044ac0d32e1c8e8812fb9f235
Lizagrebenkina <liza_grebenkina@bk.ru>
2023-02-11 16:23:15 +0300

    Send debug

--- Assets/Scripts/Menu/DiaryPanel.cs
+++ Assets/Scripts/Menu/DiaryPanel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.IO;
 
 public class DiaryPanel : MonoBehaviour
@@ -12,37 +13,42 @@ public class DiaryPanel : MonoBehaviour
     //при нажатии отменить закрывать поле для ввода и кнопки
     //при нажатии принять дописывать текст
 
-[SerializeField] private Text _text;
+    [SerializeField] private Text _text;
 
-private void Start()
-{
-    string path = Path.Combine(Application.persistentDataPath, PrefsName.DiaryFileName);
+    private void Start()
+    {
+        string path = Path.Combine(Application.persistentDataPath, PrefsName.DiaryFileName);
 
-Debug.Log(path);
-    AddedTextToFile(path, GetCurrentData());
+        Debug.Log(path);
 
-    _text.text = ReadTextFromFile(path);
-}
-
-
-    private string GetCurrentData(){
-        String.Format("{0:g}", DateTime.Now);
+        _text.text = ReadTextFromFile(path);
     }
-    
-        private string ReadTextFromFile(string path)
-        {
-            string text = "";
 
-            if (System.IO.File.Exists(path) == false) AddedTextToFile(path, "");
 
-            using (StreamReader sr = new StreamReader(path))
-                text = sr.ReadToEnd();
+    public void AddedDiaryLine(string text)
+    {
+        string path = Path.Combine(Application.persistentDataPath, PrefsName.DiaryFileName);
 
-            return text;
-        }
-        private void AddedTextToFile(string path, string text)
-        {
-            using(StreamWriter sw = new StreamWriter(path))
-                sw.WriteLine(text);
-        }
+        AddedTextToFile(path, $"{GetCurrentData()} text\n");
+    }
+
+
+    private string GetCurrentData() => $"[{String.Format("{0:g}", DateTime.Now)}]";
+
+    private string ReadTextFromFile(string path)
+    {
+        string text = "";
+
+        if (System.IO.File.Exists(path) == false) AddedTextToFile(path, "");
+
+        using (StreamReader sr = new StreamReader(path))
+            text = sr.ReadToEnd();
+
+        return text;
+    }
+    private void AddedTextToFile(string path, string text)
+    {
+        using (StreamWriter sw = new StreamWriter(path))
+            sw.WriteLine(text);
+    }
 }
